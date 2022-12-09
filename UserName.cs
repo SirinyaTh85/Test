417a660839855c36687b152fd414ee1b7ecf6114
Sirinya1085 <111561634+Sirinya1085@users.noreply.github.com>
2022-12-09 13:26:49 +0700

    first commit

---
+++ UserName.cs
@@ -0,0 +1,32 @@
+﻿class Node {
+    private char value;
+    private Node next = null;
+
+    public Node(char value) {
+        this.value = value;
+    }
+
+    public void SetNext(Node next) {
+        this.next = next;
+    }
+
+    public Node Next() {
+        return this.next;
+    }
+
+    public char GetValue() {
+        return this.value;
+    }
+}
+
+class Program {
+
+    static void Main(string[] args) {
+        Node a = new Node('A');
+        Node b = new Node('B');
+        a.SetNext(b);
+        b.SetNext(a);
+        Console.WriteLine(a.Next().Next().Next().Next().Next().GetValue());
+    }
+
+}
